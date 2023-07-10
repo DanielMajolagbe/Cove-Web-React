77e8fa7ee68c2969e8ad709e1f998203074ebddf
Daniel-Git <majolagbedaniel@gmail.com>
2023-07-10 05:42:48 -0700

    Add files via upload

---
+++ Cove Web/src/pages/GeneralLandingPage.tsx
@@ -0,0 +1,338 @@
+import { FunctionComponent } from "react";
+import styles from "./GeneralLandingPage.module.css";
+const GeneralLandingPage: FunctionComponent = () => {
+  return (
+    <div className={styles.generalLandingPage}>
+      <div className={styles.generalLandingPageChild} />
+      <img
+        className={styles.generalLandingPageItem}
+        alt=""
+        src="/vector-4.svg"
+      />
+      <div className={styles.ellipse} />
+      <div className={styles.ellipse1} />
+      <div className={styles.ellipse2} />
+      <div className={styles.ellipse3} />
+      <div className={styles.generalLandingPageInner} />
+      <div className={styles.rectangleDiv} />
+      <div className={styles.maskGroup}>
+        <div className={styles.abstractLandscapeDarkBlueB} />
+      </div>
+      <div className={styles.discoverIntelligentProfessiContainer}>
+        <span className={styles.discoverIntelligentProfessiContainer1}>
+          <span>{`Discover intelligent, professional freelance contractors and full time employees. You can also `}</span>
+          <b>get featured</b>
+        </span>
+      </div>
+      <div className={styles.findYourJob}>Find Your Job Category</div>
+      <div className={styles.connectCollaborate}>{`Connect & Collaborate`}</div>
+      <img
+        className={styles.hearFromOurUsers}
+        alt=""
+        src="/hear-from-our-users.svg"
+      />
+      <div className={styles.contact}>Contact</div>
+      <div className={styles.services}>Services</div>
+      <div className={styles.about}>About</div>
+      <div className={styles.customerCare}>{`Customer Care & Support`}</div>
+      <div className={styles.termsAndConditions}>Terms And Conditions</div>
+      <img
+        className={styles.iconMessageText}
+        alt=""
+        src="/-icon-message-text.svg"
+      />
+      <img className={styles.maskGroupIcon} alt="" src="/mask-group@2x.png" />
+      <div className={styles.maskGroup1}>
+        <b className={styles.featured}>Featured</b>
+        <img
+          className={styles.whatsappImage20230607At3}
+          alt=""
+          src="/whatsapp-image-20230607-at-350-3@2x.png"
+        />
+      </div>
+      <img className={styles.maskGroupIcon1} alt="" src="/mask-group1@2x.png" />
+      <img className={styles.maskGroupIcon2} alt="" src="/mask-group2@2x.png" />
+      <img className={styles.maskGroupIcon3} alt="" src="/mask-group3@2x.png" />
+      <img className={styles.maskGroupIcon4} alt="" src="/mask-group4@2x.png" />
+      <div className={styles.frameDiv}>
+        <div className={styles.programersParent}>
+          <div className={styles.programers}>PROGRAMERS</div>
+          <div className={styles.engineers}>ENGINEERS</div>
+          <img className={styles.groupChild} alt="" src="/star-1.svg" />
+          <img className={styles.groupItem} alt="" src="/star-1.svg" />
+          <img className={styles.groupInner} alt="" src="/star-1.svg" />
+          <img className={styles.starIcon} alt="" src="/star-1.svg" />
+          <img className={styles.groupChild1} alt="" src="/star-1.svg" />
+          <img className={styles.groupChild2} alt="" src="/star-1.svg" />
+          <div className={styles.writers}>WRITERS</div>
+          <div className={styles.editors}>EDITORS</div>
+          <div className={styles.animators}>aNIMATORS</div>
+          <div className={styles.legatPractitioners}>LEGAT PRACTITIONERS</div>
+        </div>
+      </div>
+      <b className={styles.newsletter}>Newsletter</b>
+      <img className={styles.ellipseIcon} alt="" src="/ellipse.svg" />
+      <img className={styles.ellipseIcon1} alt="" src="/ellipse1.svg" />
+      <div className={styles.generalLandingPageChild1} />
+      <div className={styles.generalLandingPageChild2} />
+      <div className={styles.subscribe}>Subscribe</div>
+      <div className={styles.enterYourEmail}>
+        Enter your Email to receive news
+      </div>
+      <div className={styles.findingTheRightContainer}>
+        <p className={styles.findingTheRight}>
+          Finding the right Jobs as a developer and finding the right developer
+          to collaborate on a project with can be challenging
+        </p>
+        <p
+          className={styles.findingTheRight}
+        >{`        but on Cove, your ideas can be implemented by the best developers right now, right here! We are Connecting `}</p>
+        <p className={styles.findingTheRight}>
+          {" "}
+          outstanding talents with the most innovative companies.
+        </p>
+      </div>
+      <b className={styles.collaborateWithTopNotch}>
+        Collaborate with top-notch developers, designers and more on your
+        project
+      </b>
+      <div className={styles.generalLandingPageInner1}>
+        <div className={styles.frameWrapper}>
+          <div className={styles.frameContainer}>
+            <div className={styles.frameWrapper1}>
+              <div className={styles.groupWrapper}>
+                <div className={styles.rectangleParent}>
+                  <div className={styles.groupChild3} />
+                  <div className={styles.getStarted}>Get Started</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.generalLandingPageChild3} />
+      <div className={styles.howItWorks}>How It Works</div>
+      <div className={styles.rectangleGroup}>
+        <div className={styles.instanceChild} />
+        <div className={styles.ellipseParent}>
+          <div className={styles.ellipseDiv} />
+          <div className={styles.div}>1</div>
+        </div>
+        <b className={styles.createAnAccount}>
+          Create an account and set up your profile
+        </b>
+        <div className={styles.creatingAnAccount}>
+          Creating an account is quick and easy, it’s the first step towards
+          finding your freelance project . Signup today and join the beta users
+          of Cove.
+        </div>
+      </div>
+      <div className={styles.rectangleContainer}>
+        <div className={styles.instanceChild} />
+        <div className={styles.instanceInner} />
+        <div className={styles.div1}>2</div>
+        <b className={styles.getMatchedWith}>Get Matched with Talent</b>
+        <div className={styles.wellMatchYouContainer}>
+          <span className={styles.discoverIntelligentProfessiContainer1}>
+            <p
+              className={styles.findingTheRight}
+            >{`We’ll match you with the right freelancer based on your project needs. We `}</p>
+            <p
+              className={styles.findingTheRight}
+            >{`have a rigorous screening process to ensure that everyone get matched with   the best possible talents. `}</p>
+          </span>
+        </div>
+      </div>
+      <div className={styles.groupDiv}>
+        <div className={styles.instanceChild} />
+        <div className={styles.instanceChild2} />
+        <div className={styles.div2}>3</div>
+        <b className={styles.startYourProject}>Start Your Project</b>
+        <div
+          className={styles.findTheRight}
+        >{`Find the right developers and start building your vision and confidence. At Cove we provide a platform for everyone to connect with skilled and experienced freelance professionals. `}</div>
+      </div>
+      <div className={styles.generalLandingPageInner2}>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.groupChild4} />
+          <div className={styles.webDeveloperAnd}>
+            Web Developer and Mobile App Developer
+          </div>
+          <b className={styles.viewJobs}>View Jobs</b>
+        </div>
+      </div>
+      <div className={styles.generalLandingPageInner3}>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.groupChild4} />
+          <div className={styles.webDeveloperAnd}>
+            Sales, Marketing and Customer support
+          </div>
+          <b className={styles.viewJobs}>View Jobs</b>
+        </div>
+      </div>
+      <div className={styles.generalLandingPageInner4}>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.groupChild4} />
+          <div className={styles.webDeveloperAnd}>
+            Product Creation and UI/UX Design
+          </div>
+          <b className={styles.viewJobs}>View Jobs</b>
+        </div>
+      </div>
+      <div className={styles.generalLandingPageInner5}>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.groupChild4} />
+          <div className={styles.webDeveloperAnd}>
+            Technical Writing, Copywrite and Translation
+          </div>
+          <b className={styles.viewJobs}>View Jobs</b>
+        </div>
+      </div>
+      <div className={styles.generalLandingPageInner6}>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.groupChild4} />
+          <div className={styles.webDeveloperAnd}>
+            Video Editing, Storyboard Artist and Visual Effect Artist
+          </div>
+          <b className={styles.viewJobs}>View Jobs</b>
+        </div>
+      </div>
+      <div className={styles.generalLandingPageInner7}>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.groupChild4} />
+          <div className={styles.webDeveloperAnd}>
+            Business Finance, Accounting and Legal
+          </div>
+          <b className={styles.viewJobs}>View Jobs</b>
+        </div>
+      </div>
+      <div className={styles.generalLandingPageInner8}>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.groupChild4} />
+          <div className={styles.webDeveloperAnd}>
+            Project Management and DevOps Engineering
+          </div>
+          <b className={styles.viewJobs}>View Jobs</b>
+        </div>
+      </div>
+      <div className={styles.generalLandingPageInner9}>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.groupChild4} />
+          <div className={styles.webDeveloperAnd}>
+            Character Animation and Compositing Artist
+          </div>
+          <b className={styles.viewJobs}>View Jobs</b>
+        </div>
+      </div>
+      <div className={styles.generalLandingPageInner10}>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.groupChild4} />
+          <div className={styles.findDevelopersNear}>
+            Find Developers near you and work on projects together
+          </div>
+        </div>
+      </div>
+      <div className={styles.generalLandingPageChild4} />
+      <div className={styles.iHadA}>
+        " I had a great experience using Cove.The interface was easy to use and
+        navigate, and I was able to find a freelancer to complete my project"
+      </div>
+      <div className={styles.generalLandingPageChild5} />
+      <img className={styles.maskGroupIcon5} alt="" src="/mask-group5@2x.png" />
+      <img className={styles.maskGroupIcon6} alt="" src="/mask-group6@2x.png" />
+      <div className={styles.damnCoveExceeded}>
+        "Damn! Cove exceeded my expectations. I will definitely use this
+        platform again for future projects!"
+      </div>
+      <b className={styles.marcus}>Marcus</b>
+      <b className={styles.jerome}>Jerome</b>
+      <img className={styles.rectangleIcon} alt="" src="/rectangle-28@2x.png" />
+      <div className={styles.theAllInOnePlatform}>
+        The all-in-one platform revolutionizing the way freelancers and
+        employers connect. Explore tech-focused job opportunities, schedule
+        interviews, and communicate seamlessly. Showcase projects, code
+        snippets, and designs. Receive personalized job recommendations,
+        participate in coding challenges, and collaborate on open-source
+        projects.
+      </div>
+      <div className={styles.postJobsBrowse}>
+        {" "}
+        Post jobs, browse listings, and feature your portfolio to stand out.
+        Streamlined interface, personalized profiles, and powerful search make
+        Cove your go-to platform for success
+      </div>
+      <div className={styles.getTestedGet}>Get Tested, get hired!</div>
+      <img className={styles.maskGroupIcon7} alt="" src="/mask-group7@2x.png" />
+      <img
+        className={styles.generalLandingPageChild6}
+        alt=""
+        src="/rectangle-34@2x.png"
+      />
+      <div className={styles.generalLandingPageChild7} />
+      <img className={styles.ellipseIcon2} alt="" src="/ellipse.svg" />
+      <img className={styles.ellipseIcon3} alt="" src="/ellipse1.svg" />
+      <div className={styles.generalLandingPageChild8} />
+      <div className={styles.findingTheRightContainer1}>
+        <p className={styles.findingTheRight}>
+          Finding the right Jobs as a developer and finding the right developer
+          to collaborate on a project with can be challenging
+        </p>
+        <p
+          className={styles.findingTheRight}
+        >{`        but on Cove, your ideas can be implemented by the best developers right now, right here! We are Connecting `}</p>
+        <p className={styles.findingTheRight}>
+          {" "}
+          outstanding talents with the most innovative companies.
+        </p>
+      </div>
+      <b className={styles.collaborateWithTopNotch1}>
+        Collaborate with top-notch developers, designers and more on your
+        project
+      </b>
+      <div className={styles.generalLandingPageInner1}>
+        <div className={styles.frameWrapper}>
+          <div className={styles.frameContainer}>
+            <div className={styles.frameWrapper1}>
+              <div className={styles.groupWrapper}>
+                <div className={styles.rectangleParent}>
+                  <div className={styles.groupChild3} />
+                  <div className={styles.getStarted}>Get Started</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.howItWorks1}>How It Works</div>
+      <div className={styles.groupParent}>
+        <div className={styles.rectangleParent11}>
+          <div className={styles.groupChild14} />
+          <div className={styles.submitPortfolio}>Submit Portfolio</div>
+          <div className={styles.rectangleParent12}>
+            <div className={styles.groupChild15} />
+            <div className={styles.getStarted2}>Get Started</div>
+          </div>
+          <div className={styles.groupChild16} />
+          <div className={styles.menuParent}>
+            <div className={styles.menu}>Menu</div>
+            <img
+              className={styles.iconArrowIosDownward}
+              alt=""
+              src="/-icon-arrow-ios-downward.svg"
+            />
+          </div>
+          <img className={styles.iconTwitter} alt="" src="/-icon-twitter.svg" />
+          <img
+            className={styles.iconLinkedin}
+            alt=""
+            src="/-icon-linkedin.svg"
+          />
+        </div>
+        <img className={styles.asset41} alt="" src="/asset-4-1@2x.png" />
+        <div className={styles.cove}>cove</div>
+      </div>
+    </div>
+  );
+};
+
+export default GeneralLandingPage;
